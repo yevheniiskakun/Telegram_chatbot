9fb08722a24c1a55f5131e9278a74997a8405395
yevhinator <yevhenii.skakun@gmail.com>
2022-02-25 21:44:59 +0100

    Small changes

--- app.py
+++ app.py
@@ -5,8 +5,14 @@ bot = telebot.TeleBot('5101749263:AAFgCKe0PzjY-HzQmpS5yAiGFeGm02UDsOU')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    send_mess = "Hello"
-    bot.send_message(message.chat.id, send_mess, parse_mode="html")
+    context = "Hello"
+    bot.send_message(message.chat.id, context, parse_mode="html")
+
+
+@bot.message_handler(content_types=['text'])
+def main(message):
+    context = message.text
+    bot.send_message(message.chat.id, context)
 
 
 bot.polling(none_stop=True)
